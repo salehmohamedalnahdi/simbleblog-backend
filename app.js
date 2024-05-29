2e6fc123c6e02222d3e488a940df42cd34a1f515
salehmohamedalnahdi <smbt2011@gmail.com>
2024-05-29 20:32:20 +0300

    fbbf

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 const blogRoutes=require('./routes/blogRoute.js')
 
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 app.use('/',blogRoutes)
 app.listen(3000,()=>console.log("connecting is done"))
 
